5da9f6f94ee7a4fe59d7f0cfe1abdb4911dd917a
César <cesiro@MacBook-Air-de-Cesar.local>
2013-12-01 11:00:10 +0100

    Removed unused classes.

--- service/src/main/java/es/zaldo/petstore/service/PetsResource.java
+++ service/src/main/java/es/zaldo/petstore/service/PetsResource.java
@@ -28,14 +28,15 @@ import es.zaldo.petstore.core.exceptions.PetsValidationException;
 import es.zaldo.petstore.core.utils.PerformanceMonitor;
 import es.zaldo.petstore.core.utils.PerformanceMonitor.AvailableMonitors;
 import es.zaldo.petstore.service.validation.BoxValidator;
-import es.zaldo.petstore.service.validation.LocationValidator;
 import es.zaldo.petstore.service.validation.PageRequestValidator;
-import es.zaldo.petstore.service.validation.RadiusValidator;
 
 /**
  * Resource used to add and search pets.
- *
- * <p>This resource will be used to adds and return pets given different parameters.</p>
+ * 
+ * <p>
+ * This resource will be used to adds and return pets given different
+ * parameters.
+ * </p>
  */
 @Path("/api/v1/pets/")
 @org.codehaus.enunciate.modules.jersey.ExternallyManagedLifecycle
@@ -53,52 +54,44 @@ public class PetsResource {
     private final MarshalHandler marshallHandler;
     private final PetMarshaller petMarshaller;
     private final BoxValidator boxValidator;
-    private final RadiusValidator radiusValidator;
-    private final LocationValidator locationValidator;
     private final PageRequestValidator pageRequestValidator;
     private final PerformanceMonitor monitor;
 
     /**
      * Constructor of the class.
-     *
-     * @param manager Manager to use to access the model
+     * 
+     * @param manager
+     *            Manager to use to access the model
      */
     public PetsResource(PetManager manager, MarshalHandler marshalHandler,
-        PetMarshaller petMarshaller,
-        LocationValidator locationValidator, BoxValidator boxValidator,
-        PageRequestValidator pageRequestValidator, RadiusValidator radiusValidator,
-            PerformanceMonitor monitor) {
+            PetMarshaller petMarshaller, BoxValidator boxValidator,
+            PageRequestValidator pageRequestValidator, PerformanceMonitor monitor) {
         this.manager = manager;
         this.marshallHandler = marshalHandler;
         this.petMarshaller = petMarshaller;
         this.boxValidator = boxValidator;
-        this.radiusValidator = radiusValidator;
-        this.locationValidator = locationValidator;
         this.pageRequestValidator = pageRequestValidator;
         this.monitor = monitor;
     }
 
     /**
      * Returns a pet object.
-     *
+     * 
      * @return Response containing a pet object.
      */
     @GET
     @Path("/pet/{id}")
-    @Produces({
-        MediaType.APPLICATION_JSON + Constants.CHARSET_UTF8,
-        MediaType.TEXT_XML + Constants.CHARSET_UTF8
-    })
+    @Produces({ MediaType.APPLICATION_JSON + Constants.CHARSET_UTF8,
+            MediaType.TEXT_XML + Constants.CHARSET_UTF8 })
     @org.codehaus.enunciate.jaxrs.StatusCodes({
-        @org.codehaus.enunciate.jaxrs.ResponseCode (code = 200, condition = "Everything is OK."),
-        @org.codehaus.enunciate.jaxrs.ResponseCode (code = 404, condition = "If no pet is found with the given ID."),
-        @org.codehaus.enunciate.jaxrs.ResponseCode (code = 500, condition = "If any unexpected error happens.")
-    })
+            @org.codehaus.enunciate.jaxrs.ResponseCode(code = 200, condition = "Everything is OK."),
+            @org.codehaus.enunciate.jaxrs.ResponseCode(code = 404, condition = "If no pet is found with the given ID."),
+            @org.codehaus.enunciate.jaxrs.ResponseCode(code = 500, condition = "If any unexpected error happens.") })
     public Response getPet(@PathParam("id") final String id,
             @QueryParam("debug") final boolean debug) {
         try {
             monitor.start(AvailableMonitors.Load_Pet_Core);
-            Pet pet= manager.loadById(id);
+            Pet pet = manager.loadById(id);
             monitor.stop(AvailableMonitors.Load_Pet_Core);
 
             monitor.start(AvailableMonitors.Marshal_Pet);
@@ -107,8 +100,10 @@ public class PetsResource {
 
             ResponseBuilder builder = Response.status(Status.OK).entity(json);
             if (debug) {
-                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Load_Pet_Core, monitor.getShortData(AvailableMonitors.Load_Pet_Core));
-                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Marshal_Pet, monitor.getShortData(AvailableMonitors.Marshal_Pet));
+                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Load_Pet_Core,
+                        monitor.getShortData(AvailableMonitors.Load_Pet_Core));
+                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Marshal_Pet,
+                        monitor.getShortData(AvailableMonitors.Marshal_Pet));
             }
 
             return builder.build();
@@ -122,22 +117,18 @@ public class PetsResource {
 
     /**
      * Update an existing pet object.
-     *
+     * 
      * @return Response containing the pet object updated.
      */
     @POST
     @Path("/pet")
-    @Produces({
-        MediaType.APPLICATION_JSON + Constants.CHARSET_UTF8,
-        MediaType.TEXT_XML + Constants.CHARSET_UTF8
-    })
+    @Produces({ MediaType.APPLICATION_JSON + Constants.CHARSET_UTF8,
+            MediaType.TEXT_XML + Constants.CHARSET_UTF8 })
     @org.codehaus.enunciate.jaxrs.StatusCodes({
-        @org.codehaus.enunciate.jaxrs.ResponseCode (code = 200, condition = "Everything is OK."),
-        @org.codehaus.enunciate.jaxrs.ResponseCode (code = 400, condition = "If there is a problem validating the request (missing parameters, invalid types, etc.)."),
-        @org.codehaus.enunciate.jaxrs.ResponseCode (code = 500, condition = "If any unexpected error happens.")
-    })
-    public Response updatePet(JSONObject jsonObject,
-            @QueryParam("debug") final boolean debug) {
+            @org.codehaus.enunciate.jaxrs.ResponseCode(code = 200, condition = "Everything is OK."),
+            @org.codehaus.enunciate.jaxrs.ResponseCode(code = 400, condition = "If there is a problem validating the request (missing parameters, invalid types, etc.)."),
+            @org.codehaus.enunciate.jaxrs.ResponseCode(code = 500, condition = "If any unexpected error happens.") })
+    public Response updatePet(JSONObject jsonObject, @QueryParam("debug") final boolean debug) {
         try {
 
             monitor.start(AvailableMonitors.Unmarshal_Pet);
@@ -145,7 +136,7 @@ public class PetsResource {
             monitor.stop(AvailableMonitors.Unmarshal_Pet);
 
             monitor.start(AvailableMonitors.Update_Pet_Core);
-            Pet newPet= manager.update(pet);
+            Pet newPet = manager.update(pet);
             monitor.stop(AvailableMonitors.Update_Pet_Core);
 
             monitor.start(AvailableMonitors.Marshal_Pet);
@@ -154,9 +145,12 @@ public class PetsResource {
 
             ResponseBuilder builder = Response.status(Status.OK).entity(json);
             if (debug) {
-                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Unmarshal_Pet, monitor.getShortData(AvailableMonitors.Unmarshal_Pet));
-                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Update_Pet_Core, monitor.getShortData(AvailableMonitors.Update_Pet_Core));
-                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Marshal_Pet, monitor.getShortData(AvailableMonitors.Marshal_Pet));
+                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Unmarshal_Pet,
+                        monitor.getShortData(AvailableMonitors.Unmarshal_Pet));
+                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Update_Pet_Core,
+                        monitor.getShortData(AvailableMonitors.Update_Pet_Core));
+                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Marshal_Pet,
+                        monitor.getShortData(AvailableMonitors.Marshal_Pet));
             }
 
             return builder.build();
@@ -170,25 +164,22 @@ public class PetsResource {
 
     /**
      * Get pets by their <code>id<code>s.
-     *
+     * 
      * <p><code>id</code> parameter can be specified multiple times.</p>
-     *
-     * @param ids pets IDs. Optional parameter, the default value is null.
-     *
+     * 
+     * @param ids
+     *            pets IDs. Optional parameter, the default value is null.
+     * 
      * @return Response containing a list of pet objects.
      */
     @GET
     @Path("/byid")
-    @Produces({
-        MediaType.APPLICATION_JSON + Constants.CHARSET_UTF8,
-        MediaType.TEXT_XML + Constants.CHARSET_UTF8
-    })
+    @Produces({ MediaType.APPLICATION_JSON + Constants.CHARSET_UTF8,
+            MediaType.TEXT_XML + Constants.CHARSET_UTF8 })
     @org.codehaus.enunciate.jaxrs.StatusCodes({
-        @org.codehaus.enunciate.jaxrs.ResponseCode (code = 200, condition = "Everything is OK."),
-        @org.codehaus.enunciate.jaxrs.ResponseCode (code = 500, condition = "If any unexpected error happens.")
-    })
-    public Response getPetsById(
-            @QueryParam("id") @DefaultValue("") final List<String> ids,
+            @org.codehaus.enunciate.jaxrs.ResponseCode(code = 200, condition = "Everything is OK."),
+            @org.codehaus.enunciate.jaxrs.ResponseCode(code = 500, condition = "If any unexpected error happens.") })
+    public Response getPetsById(@QueryParam("id") @DefaultValue("") final List<String> ids,
             @QueryParam("debug") final boolean debug) {
         try {
             monitor.start(AvailableMonitors.Get_Pets_Core);
@@ -201,8 +192,10 @@ public class PetsResource {
 
             ResponseBuilder builder = Response.status(Status.OK).entity(json);
             if (debug) {
-                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Get_Pets_Core, monitor.getShortData(AvailableMonitors.Get_Pets_Core));
-                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Marshal_List, monitor.getShortData(AvailableMonitors.Marshal_List));
+                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Get_Pets_Core,
+                        monitor.getShortData(AvailableMonitors.Get_Pets_Core));
+                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Marshal_List,
+                        monitor.getShortData(AvailableMonitors.Marshal_List));
             }
             return builder.build();
         } catch (Exception e) {
@@ -211,31 +204,35 @@ public class PetsResource {
         }
     }
 
-
     /**
      * Search pets by bounds
-     *
-     * @param nwLatitude    Northwest latitude. Mandatory parameter.
-     * @param nwLongitude   Northwest longitude. Mandatory parameter.
-     * @param seLatitude    Southeast latitude. Mandatory parameter.
-     * @param seLongitude   Southeast longitude. Mandatory parameter.
-     * @param group     pet group. Optional parameter, the default value is null.
-     * @param type      pet type. Optional parameter, the default value is null.
-     * @param page Pagination start page. Default value is 0
-     * @param size Num of rows to retrieve. Default value is 10
-     *
+     * 
+     * @param nwLatitude
+     *            Northwest latitude. Mandatory parameter.
+     * @param nwLongitude
+     *            Northwest longitude. Mandatory parameter.
+     * @param seLatitude
+     *            Southeast latitude. Mandatory parameter.
+     * @param seLongitude
+     *            Southeast longitude. Mandatory parameter.
+     * @param group
+     *            pet group. Optional parameter, the default value is null.
+     * @param type
+     *            pet type. Optional parameter, the default value is null.
+     * @param page
+     *            Pagination start page. Default value is 0
+     * @param size
+     *            Num of rows to retrieve. Default value is 10
+     * 
      * @return Response containing a list of pet objects.
      */
     @GET
     @Path("/search/within")
-    @Produces({
-        MediaType.APPLICATION_JSON + Constants.CHARSET_UTF8,
-        MediaType.TEXT_XML + Constants.CHARSET_UTF8
-    })
+    @Produces({ MediaType.APPLICATION_JSON + Constants.CHARSET_UTF8,
+            MediaType.TEXT_XML + Constants.CHARSET_UTF8 })
     @org.codehaus.enunciate.jaxrs.StatusCodes({
-        @org.codehaus.enunciate.jaxrs.ResponseCode (code = 200, condition = "Everything is OK."),
-        @org.codehaus.enunciate.jaxrs.ResponseCode (code = 500, condition = "If any unexpected error happens.")
-    })
+            @org.codehaus.enunciate.jaxrs.ResponseCode(code = 200, condition = "Everything is OK."),
+            @org.codehaus.enunciate.jaxrs.ResponseCode(code = 500, condition = "If any unexpected error happens.") })
     public Response searchWithin(
             @QueryParam("nwlat") @DefaultValue(Constants.INVALID_COORDINATE_VALUE) final double nwLatitude,
             @QueryParam("nwlon") @DefaultValue(Constants.INVALID_COORDINATE_VALUE) final double nwLongitude,
@@ -245,20 +242,17 @@ public class PetsResource {
             @QueryParam("type") final List<String> types,
             @QueryParam("page") @DefaultValue(Constants.DEFAULT_START_PAGE) final int page,
             @QueryParam("size") @DefaultValue(Constants.DEFAULT_PAGE_NUMROWS) final int size,
-            @QueryParam("debug") final boolean debug
-        ) {
+            @QueryParam("debug") final boolean debug) {
 
-        Box bounds = new Box(
-                new Location(nwLatitude, nwLongitude),
-                new Location(seLatitude, seLongitude));
+        Box bounds = new Box(new Location(nwLatitude, nwLongitude), new Location(seLatitude,
+                seLongitude));
         if (!boxValidator.isValid(bounds)) {
             throw new DataValidationException(MSG_INVALID_BOX);
         }
 
         PageRequest pagination = new PageRequest(page, size);
         if (!pageRequestValidator.isValid(pagination)) {
-            throw new DataValidationException(
-                getInvalidPaginationErrorMessage());
+            throw new DataValidationException(getInvalidPaginationErrorMessage());
         }
 
         try {
@@ -267,13 +261,15 @@ public class PetsResource {
             monitor.stop(AvailableMonitors.Within_Core);
 
             monitor.start(AvailableMonitors.Marshal_List);
-            JSONObject json= marshallHandler.marshal(result);
+            JSONObject json = marshallHandler.marshal(result);
             monitor.stop(AvailableMonitors.Marshal_List);
 
             ResponseBuilder builder = Response.status(Status.OK).entity(json);
             if (debug) {
-                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Within_Core, monitor.getShortData(AvailableMonitors.Within_Core));
-                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Marshal_List, monitor.getShortData(AvailableMonitors.Marshal_List));
+                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Within_Core,
+                        monitor.getShortData(AvailableMonitors.Within_Core));
+                builder.header(PetsResource.DEBUG_PREFIX + AvailableMonitors.Marshal_List,
+                        monitor.getShortData(AvailableMonitors.Marshal_List));
             }
             return builder.build();
         } catch (Exception e) {
@@ -284,11 +280,11 @@ public class PetsResource {
 
     /**
      * @return The error message to be shown if a pagination request is not
-     * valid.
+     *         valid.
      */
     private String getInvalidPaginationErrorMessage() {
-        return MSG_MAX_PAGE + pageRequestValidator.getMaxPage() + ", " +
-            MAX_PAGE_SIZE + pageRequestValidator.getMaxSize();
+        return MSG_MAX_PAGE + pageRequestValidator.getMaxPage() + ", " + MAX_PAGE_SIZE
+                + pageRequestValidator.getMaxSize();
     }
 
 }
